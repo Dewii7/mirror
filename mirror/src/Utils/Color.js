45cc89b069e4795adc0d233b8b2f0f007eca4070
Dewi Anggraini <goddess.guy@gmail.com>
2021-08-17 16:18:46 +0800

    Utils Colors

---
+++ mirror/src/Utils/Color.js
@@ -0,0 +1,9 @@
+export const COLORS = {
+  red: '#FF385C',
+  black: '#4A4A4A',
+  softBlack: '#CCCCCC',
+  white: '#FFFFFF',
+  yellow: '#F2C94C',
+  yellowBorder: '#F2C94C33',
+  grey: '#AAAAAA',
+};
